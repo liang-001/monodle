673e47c7796bf95aafddb75937df381623cc1dc6
liang-001 <70849790+liang-001@users.noreply.github.com>
2021-06-27 09:05:12 +0800

    Update dataloader_helper.py

--- lib/helpers/dataloader_helper.py
+++ lib/helpers/dataloader_helper.py
@@ -9,7 +9,7 @@ def my_worker_init_fn(worker_id):
     np.random.seed(np.random.get_state()[1][0] + worker_id)
 
 
-def build_dataloader(cfg, workers=4):
+def build_dataloader(cfg, workers=0):
     # perpare dataset
     if cfg['type'] == 'KITTI':
         train_set = KITTI_Dataset(split='train', cfg=cfg)
